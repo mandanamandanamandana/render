b162cdf4a9495bf48f95f98d35694a3265ee67c3
MandanaMandanaMandana <mandy7373@gmail.com>
2022-11-30 20:03:30 -0500

    class

---
+++ server.js
@@ -0,0 +1,15 @@
+require("dotenv").config();
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT;
+
+app.get('/', (request, response) => {
+    response.send(`hello world i am listening to port ${PORT}`);
+});
+
+app.get('/dburl', (request, response) => {
+    response.send(`My connection string is: ${process.env.DATABASE_URL}`)
+});
+
+app.listen(PORT, () => {
+    console.log(`✅ PORT: ${PORT} 🌟`)
